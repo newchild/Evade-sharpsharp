ea2609d8cd47be584230ba0162f376e3860ca544
newchild <fabianbrand98@gmx.de>
2015-02-03 16:43:29 +0100

    Holy fuck

--- Evadesharpsharp/Program.cs
+++ Evadesharpsharp/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using LeagueSharp;
 using SharpDX;
+using SpellDetector;
 using System.Drawing;
 using LeagueSharp.Common;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Evadesharpsharp
 	{
 		private static Menu _Menu;
 		private static Obj_AI_Hero Player;
+		private static bool isEvading = false;
 		static void Main(string[] args)
 		{
 			CustomEvents.Game.OnGameLoad+=Game_OnGameLoad;
@@ -21,6 +23,7 @@ namespace Evadesharpsharp
 
 		private static void Game_OnGameLoad(EventArgs args)
 		{
+			Player = ObjectManager.Player;
 			_Menu = new Menu("Evade##","evade",true);
 			var commonMenu = new Menu("General","evade.general");
 			commonMenu.AddItem(new MenuItem("evade.general.printinfo","Print Info in chat").SetValue(true));
@@ -29,18 +32,19 @@ namespace Evadesharpsharp
 			Game.PrintChat("Evade## loaded");
 			Game.OnGameUpdate += Game_OnGameUpdate;
 			Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
-			GameObject.OnCreate+=GameObject_OnCreate;
+			Obj_AI_Hero.OnIssueOrder += Obj_AI_Hero_OnIssueOrder;
 		}
 
-		private static void GameObject_OnCreate(GameObject sender, EventArgs args)
-		{
-			Game.Say("/all general spells isVisible?: " + sender.IsVisible);
-		}
+		
 
 		static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
 		{
-			if (!sender.IsMinion && !args.SData.Name.StartsWith("SRUAP"))
-				Game.Say("/all Spell fired at x: " + args.Start.X + " y: " + args.Start.Y + " z: " + args.Start.Z + " visible? " + sender.IsVisible + " Spellname: " + args.SData.Name);
+			SpellDetector.Skillshots.SkillshotData data = new SpellDetector.Skillshots.SkillshotData();
+			SpellDetector.Skillshots.Skillshot help = new SpellDetector.Skillshots.Skillshot(SpellDetector.Skillshots.DetectionType.ProcessSpell, data, (int)args.TimeCast, args.Start.To2D(), args.End.To2D(), sender);
+			if (help.IsDanger(Player.Position.To2D()))
+			{
+				moveToBestLocation(sender,args,help);
+			};
 		}
 
 		
@@ -49,6 +53,40 @@ namespace Evadesharpsharp
 		static void Game_OnGameUpdate(EventArgs args)
 		{
 			
+		}
+		static void Obj_AI_Hero_OnIssueOrder(Obj_AI_Base sender, GameObjectIssueOrderEventArgs args)
+		{
+			if (isEvading)
+			{
+				args.Process = false;
+			}
+		}
+		static void moveToBestLocation(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args, SpellDetector.Skillshots.Skillshot skill)
+		{
+			Vector2 bufferVector = sender.Position.To2D();
+			for (int i = -100; i <= 100; i++)
+			{
+				for (int j = -100; j <= 100; j++)
+				{
+					bufferVector.X = sender.Position.To2D().X + i;
+					bufferVector.Y = sender.Position.To2D().Y + j;
+					if (!skill.IsDanger(bufferVector) && bufferVector.IsValid() && !bufferVector.IsWall())
+					{
+						Player.IssueOrder(GameObjectOrder.MoveTo, bufferVector.To3D());
+						isEvading = true;
+						while(Player.IsMoving){
+
+						}
+						isEvading = false;
+						goto End;
+					}
+
+				}
+			}
+			Game.PrintChat("Couldn't find evade spot");
+			return;
+		End:
+			Game.PrintChat("Successfully evaded!");
 		}
 	}
 }
